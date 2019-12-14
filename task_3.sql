6ab1e8f139f5ede2b43502920536101987bc93b7
Anton <asidorov90@gmail.com>
2019-12-14 23:46:22 +0300

    done 3 tasks

---
+++ task_3.sql
@@ -0,0 +1,31 @@
+SELECT avg_from_query.avg_from, avg_to_query.avg_to, avg_sum_query.avg_sum
+FROM
+       (SELECT area_id,
+               (sum(
+                    case when compensation_gross
+                         then compensation_from
+                         else compensation_from * 0.87 end
+                    ) / count(*)) as avg_from
+       FROM vacancy_body
+       GROUP BY area_id) as avg_from_query
+       JOIN
+       (SELECT area_id,
+               (sum(
+                    case when compensation_gross
+                         then compensation_to
+                         else compensation_to * 0.87 end
+                    ) / count(*)) as avg_to from vacancy_body WHERE compensation_to <> 0
+       GROUP BY area_id) as avg_to_query
+       USING (area_id)
+       JOIN
+       (SELECT area_id,
+               (sum(
+                    ((case when compensation_gross
+                         then compensation_to
+                         else compensation_to * 0.87 end) +
+                    (case when compensation_gross
+                         then compensation_from
+                         else compensation_from * 0.87 end)) / 2
+                    ) / count(*)) as avg_sum from vacancy_body WHERE compensation_to <> 0
+       GROUP BY area_id) as avg_sum_query
+       USING (area_id);
